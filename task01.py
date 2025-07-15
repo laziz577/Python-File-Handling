8d22d1fadf7f57f5140ea65a2f4023cc7d3874f5
laziz577 <temirovabdulaziz042@gmail.com>
2025-07-15 20:28:53 +0500

    bajarildi

---
+++ task01.py
@@ -0,0 +1,17 @@
+def read_data(file_path: str)-> str:
+    f = open(file_path)
+    content = f.read()
+    f.close()
+    return content
+
+def write_data(file_path:str,data:str):
+    f = open(file_path,'w')
+    f.write(data)
+    f.close()
+file_path = "Input/numbers.txt"
+content = read_data(file_path)
+ 
+file_path = "Output/output01.txt" 
+data = ", ".join(content.split())   
+write_data(file_path,data)
+
